548bc58219a0a1fff59d3a1de68d558ec70c583c
Adzi-Tay Emmanuel <emmanuel.adzi-tay@kibo.school>
2023-10-15 16:56:52 +0000

    fixed some bugs

--- app.py
+++ app.py
@@ -3,12 +3,23 @@ app = Flask(__name__)
 
 @app.route('/fizzbuzz')
 def fizzbuzz():
+    digits = []
     for i in range(1,101):
-        if i % 3 == 0 and i % 5 == 0:
-            return render_template('FizzBuzz_template.html', item= "FizzBuzz")
-        elif i % 3 == 0:
-            return render_template('FizzBuzz_template.html', item= "Fizz")
-        elif i % 5 == 0:
-            return render_template('FizzBuzz_template.html', item= "Buzz")
+        digits.append(i)
+
+    outcome = []
+
+    for digit in digits:
+        if digit % 3 == 0 and digit % 5 == 0:
+            outcome.append("FizzBuzz")
+        elif digit % 3 == 0:
+            outcome.append("Fizz")
+        elif digit % 5 == 0:
+            outcome.append("Buzz")
         else:
-            return render_template('FizzBuzz_template.html', name = i)
+            outcome.append(digit)
+
+    return render_template('FizzBuzz_template.html', result = outcome)
+
+if __name__ == '__main__':
+    app.run()
